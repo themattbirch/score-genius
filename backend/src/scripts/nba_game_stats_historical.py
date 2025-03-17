d675dd2eebda24b83ff3ac9af88bb2b759d6dbc2
Matt B <matt@optimizewebsolutions.com>
2025-03-17 00:13:38 -0400

    More updates

--- backend/src/scripts/nba_game_stats_historical.py
+++ backend/src/scripts/nba_game_stats_historical.py
@@ -276,8 +276,8 @@ def process_day(date_obj: datetime):
 ##############################################################################
 
 def main():
-    start_date = datetime(2025, 3, 4)
-    end_date = datetime(2025, 3, 9)
+    start_date = datetime(2025, 3, 13)
+    end_date = datetime(2025, 3, 16)
 
     print(f"Starting historical data import from {start_date} to {end_date}")
     current = start_date
