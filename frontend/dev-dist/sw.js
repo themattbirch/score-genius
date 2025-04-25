a23816e514df2ec3f09e2617046935f2b7daa141
Matt B <matt@optimizewebsolutions.com>
2025-04-25 06:34:07 -0400

    Server change

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "app.html",
-    "revision": "0.e4rccdag0lo"
+    "revision": "0.s06adkhrf9g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("app.html"), {
