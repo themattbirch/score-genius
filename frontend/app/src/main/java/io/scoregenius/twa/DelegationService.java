509d35be1c8879aac7f172f4a3cfd62f4b257d67
Matt B <matt@optimizewebsolutions.com>
2025-04-25 07:52:04 -0400

    Server upgrade

---
+++ frontend/app/src/main/java/io/scoregenius/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package io.scoregenius.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
