0034481654b0bb37e261a133e551925760d12d1f
Matt B <matt@optimizewebsolutions.com>
2025-05-12 01:36:21 -0400

    Server test 5

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,6 @@ import { VitePWA } from "vite-plugin-pwa";
 import { resolve } from "path";
 
 export default defineConfig({
-  base: "/app/",
   plugins: [
     react(),
     /* ---------- PWA  (scoped to /app) ---------- */
