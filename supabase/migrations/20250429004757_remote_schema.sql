f75f9c2d8f6cd5ca8f65f00cb945f91be5f78bb5
Matt B <matt@optimizewebsolutions.com>
2025-05-29 02:28:03 -0400

    rm timeout settings

--- supabase/migrations/20250429004757_remote_schema.sql
+++ supabase/migrations/20250429004757_remote_schema.sql
@@ -5,10 +5,6 @@
 -- Dumped from database version 15.8
 -- Dumped by pg_dump version 17.4
 
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET transaction_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SELECT pg_catalog.set_config('search_path', '', false);
