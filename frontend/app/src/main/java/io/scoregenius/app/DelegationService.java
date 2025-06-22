61f998acf59b736bae7126d34c054781dfa48d12
Matt B <matt@optimizewebsolutions.com>
2025-06-22 02:36:09 -0400

    Assetlinks

---
+++ frontend/app/src/main/java/io/scoregenius/app/DelegationService.java
@@ -0,0 +1,14 @@
+package io.scoregenius.app;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
