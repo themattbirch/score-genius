94a145d614ed4a0f27d1bfc1100566d65a1efb77
Matt B <matt@optimizewebsolutions.com>
2025-06-24 15:11:50 -0400

    Abstract away Firebase secrets

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -12,13 +12,13 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics, logEvent } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBwKUxO1kumvltOXrCvXISwNjWSw2D7S90",
-  authDomain: "scoregenius-ddbbe.firebaseapp.com",
-  projectId: "scoregenius-ddbbe",
-  storageBucket: "scoregenius-ddbbe.firebasestorage.app",
-  messagingSenderId: "462146250938",
-  appId: "1:462146250938:web:e1fa4e5e8144ebbec0fa83",
-  measurementId: "G-MGBC7QHT01",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
