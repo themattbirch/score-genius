ba259255a6f78661bef5015a280ce76f32932879
Matt B <matt@optimizewebsolutions.com>
2025-08-07 03:16:25 -0400

    add msix build pkg

--- frontend/app/src/main/java/io/scoregenius/app/LauncherActivity.java
+++ frontend/app/src/main/java/io/scoregenius/app/LauncherActivity.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -20,27 +20,23 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 
-public class LauncherActivity
-    extends com.google.androidbrowserhelper.trusted.LauncherActivity {
+public class LauncherActivity extends com.google.androidbrowserhelper.trusted.LauncherActivity {
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    // Setting an orientation crashes the app due to the transparent background on Android 8.0
-    // Oreo and below. We only set the orientation on Oreo+; this only affects the
-    // splash screen and Chrome will still respect the orientation.
-    // See https://github.com/GoogleChromeLabs/bubblewrap/issues/496 for details.
-    if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O) {
-      setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
-    } else {
-      setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Setting an orientation crashes the app due to the transparent
+        // background on Android 8.0 (Oreo) and below. We only set the
+        // orientation on Oreo and above. This only affects the splash
+        // screen and Chrome will still respect the orientation.
+        // See https://github.com/GoogleChromeLabs/bubblewrap/issues/496 for details.
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
     }
-  }
 
-  @Override
-  protected Uri getLaunchingUrl() {
-    // Get the original launch Url.
-    Uri uri = super.getLaunchingUrl();
-    return uri;
-  }
+    @Override
+    protected Uri getLaunchingUrl() {
+        // Get the original launch URL.
+        Uri uri = super.getLaunchingUrl();
+        return uri;
+    }
 }
