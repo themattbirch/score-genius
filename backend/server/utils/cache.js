c484fff54884a1874eda1a56959c727174c94347
Matt Birch <your@email.com>
2025-09-05 02:20:40 -0400

    Remove backend/ and models/ directories from public repo

--- backend/server/utils/cache.js
+++
@@ -1,9 +0,0 @@
-// backend/server/utils/cache.js
-import NodeCache from "node-cache"; // Use import
-
-// Create a new cache instance
-const cache = new NodeCache({ stdTTL: 600, checkperiod: 120 });
-
-console.log("Cache instance created.");
-
-export default cache; // Use export default
